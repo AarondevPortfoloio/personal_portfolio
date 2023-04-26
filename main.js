aa2d9ebb865b340904ef8bd18d65af1895930475
AarondevPortfoloio <88754367+AarondevPortfoloio@users.noreply.github.com>
2023-04-26 18:06:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,16 @@
 let button = document.getElementById('button');
 let hidden = document.getElementById('hiddenElement');
 let hidden2 = document.getElementById('hiddenElement2');
+let count =0
 function btnFunction(){
 hidden.style.display='block';
 hidden2.style.display='block';
-
+button.innerHTML='Bye Flower'
+count++
+if (count==2){
+hidden.style.display='none';
+hidden2.style.display='none';
+}
 }
 
 function btnFunction2(){
@@ -32,5 +38,31 @@ hidden2.style.display='block';
 
 
 
+button.addEventListener('click',btnFunction)
+*/
+
+
+
+
+
+/*
+let button = document.getElementById('button');
+let hidden = document.getElementById('hiddenElement');
+let hidden2 = document.getElementById('hiddenElement2');
+function btnFunction(){
+hidden.style.display='block';
+hidden2.style.display='block';
+
+}
+
+function btnFunction2(){
+    button.innerHTML='Bye Flower'
+    button.onclick = function(){
+       hidden.style.display='none';
+       hidden2.style.display='none';
+       button.style.display='none';
+    }
+}
+button.addEventListener('dblclick',btnFunction2)
 button.addEventListener('click',btnFunction)
 */
