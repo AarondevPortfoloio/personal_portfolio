fe9baea3c0e32fd4689badb4a7ef22a9df86aeb7
AarondevPortfoloio <88754367+AarondevPortfoloio@users.noreply.github.com>
2023-04-26 16:21:52 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+let button = document.getElementById('button');
+let hidden = document.getElementById('hiddenElement');
+let hidden2 = document.getElementById('hiddenElement2');
+function btnFunction(){
+hidden.style.display='block';
+hidden2.style.diplsy='block';
+
+}
+
+
+
+
+
+button.addEventListener('click',btnFunction)
