f09542e24482ec6cb3dce849c8b326f8fbe11480
AarondevPortfoloio <88754367+AarondevPortfoloio@users.noreply.github.com>
2023-04-26 18:19:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ let count =0
 function btnFunction(){
 hidden.style.display='block';
 hidden2.style.display='block';
-button.innerHTML='Bye Flower'
+button.innerHTML='Bye Flower!'
 count++
 if (count==2){
 hidden.style.display='none';
 hidden2.style.display='none';
+button.style.display ='none';
 }
 }
 button.addEventListener('click',btnFunction)
